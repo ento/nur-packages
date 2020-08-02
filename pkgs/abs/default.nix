50805e0b1fa80637a4e8f1d8525d7279b71dbbed
ento <ento+github@i.pearlwaffles.xyz>
2020-08-02 13:54:52 -0800

    Update abs-lang to 2.2.2

--- pkgs/abs/default.nix
+++ pkgs/abs/default.nix
@@ -1,6 +1,6 @@
 { lib, buildGoModule, fetchFromGitHub, ... }:
 
-let version = "1.8.0"; in
+let version = "2.2.2"; in
 
 buildGoModule {
   pname = "abs";
@@ -12,14 +12,13 @@ buildGoModule {
     rev = "${version}";
     # You can get this value by running:
     # nix-prefetch-url --unpack --type sha256 https://github.com/abs-lang/abs/archive/${version}.tar.gz
-    sha256 = "0sspywx3bdjrmvnknr7qp1k3fnbklfjv8psnr3jykpykskqnmyn1";
+    #sha256 = lib.fakeSha256;
+    sha256 = "15h51x58myvysn3fdkia87wznrr2g9zn0wv7mdqaczvpz3zl4xva";
   };
 
-  goPackagePath = "github.com/abs-lang/abs";
-
   # Use this line to use a fake sha when updating to a new version:
-  #modSha256 = lib.fakeSha256;
-  modSha256 = "15cfhjw0r1d1156bk3kd5nwyx3gjcwharps1y1s2c6zg021m2lm4";
+  #vendorSha256 = lib.fakeSha256;
+  vendorSha256 = "0waawwbz6mmr9vv1qil59x1v6803zlpc0ls9mdxc6fbip4mrwicp";
 
   meta = {
     description = "ABS programming language: the joy of shell scripting.";
